d25e5d97a54b4e49defd50e4a6cede259e1267d9
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-10-30 08:55:24 -0700

    finished setting up useForm and got it to work with checkoutForm

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,12 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = (initialValue) => {
+    const [values, setValue] = useState(initialValue);
+
+    const handleChanges = (e) => {
+        setValue({ ...values, [e.target.name]: e.target.value });
+    };
+
+    return([values, handleChanges]);
+}
